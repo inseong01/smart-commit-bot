2cbabfa2f57ada26f5c6aad1c1c7ee30f2badbdb
inseong01 <insung940@naver.com>
2025-05-13 12:27:52 +0900

    optimize: 분석 항목 최소화
    - diff, status 파일 내용 필터링 추가

--- auto/bash/experiment.sh
+++ auto/bash/experiment.sh
@@ -1,7 +1,13 @@
+git reset
+git add .
+
 # file-status 제외 목록 확인
-# FILE_STATUS='./file-status.txt'
-# cat "$FILE_STATUS" | grep -vE 'work-log.txt|.gitignore|.package-lock.json|.prettierignore'
+FILE_STATUS='./file-status.txt'
+cat "$FILE_STATUS" | grep -vE 'work-log.txt|.*ignore|.package-lock.json' > file-status-1.txt
+cat "$FILE_STATUS" | grep -vE 'work-log.txt|.package-lock.json' > file-status-2.txt
 
 # diff-file 제외 목록 확인
-git diff --cached -- . ":!$WORK_LOG_FILE" ":!.gitignore" ":!package-lock.json" ":!.prettierignore" > diff-files.txt 
-git diff --cached -- . ":!$WORK_LOG_FILE" ":!.*ignore" ":!package-lock.json" > diff-files.txt 
+# WORK_LOG_FILE="work-log.txt" 
+# git diff --cached -- . ":!$WORK_LOG_FILE" ":!.gitignore" > diff-files-1.txt 
+# git diff --cached -- . ":!$WORK_LOG_FILE" ":!.gitignore" ":!package-lock.json" ":!.prettierignore" > diff-files-2.txt 
+# git diff --cached -- . ":!$WORK_LOG_FILE" ":!.*ignore" ":!package-lock.json" ":!node_modules" > diff-files-3.txt 
